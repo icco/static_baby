0764b3e26322ef0d92c6f87e52a8d20a7bb06281
Nat Welch <nat@natwelch.com>
2014-01-19 10:57:33 -0800

    Research

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 """A web app that takes a domain and creates a bucket for that user in their GCS."""
 
-from google.appengine.api import users
-
+import logging
+import json
 import os
 import cloudstorage as gcs
 import webapp2
+
+from google.appengine.api import oauth
+from google.appengine.api import app_identity
+from google.appengine.api import urlfetch
 
 my_default_retry_params = gcs.RetryParams(
     initial_delay=0.2,
@@ -17,9 +21,16 @@ gcs.set_default_retry_params(my_default_retry_params)
 class MainPage(webapp2.RequestHandler):
 
   def get(self):
-    user = users.get_current_user()
+    user = oauth.get_current_user()
     if not user:
       self.redirect(users.create_login_url(self.request.uri))
 
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
+
+# Things to look at:
+# https://github.com/GoogleCloudPlatform/storage-appengine-photos-python/blob/master/home.py
+# https://developers.google.com/appengine/docs/python/googlecloudstorageclient/
+# https://github.com/GoogleCloudPlatform/storage-file-transfer-json-python/blob/master/chunked_transfer.py
+# https://github.com/GoogleCloudPlatform/storage-oauth2-tool-python/blob/master/gs-oauth.py
+# https://developers.google.com/appengine/docs/python/appidentity/
