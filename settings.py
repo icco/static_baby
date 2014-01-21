3e0174024645d78052322a653ddb7fd590f59a8a
Nat Welch <nat@natwelch.com>
2014-01-20 19:59:05 -0800

    comitting this for the sake of testing.

---
+++ settings.py
@@ -0,0 +1,3 @@
+CLIENT_ID='394747896624-u9mqhnkq51putl1bq2nqdhg377i57e9c.apps.googleusercontent.com'
+CLIENT_SECRET='JvsaworHI1IwwBi5fnwjYIMF'
+SCOPE='https://www.googleapis.com/auth/tasks.readonly'
